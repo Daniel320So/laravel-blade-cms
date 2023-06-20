7a17e2f263f4a39197f90af45dd8347935d7bfa5
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-06-20 17:08:33 -0400

    Remove unused table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Type;
 use App\Models\Project;
-use App\Models\Entry;
 use App\Models\Skill;
 use App\Models\ProjectSkill;
 use App\Models\Application;
@@ -22,17 +20,13 @@ class DatabaseSeeder extends Seeder
     {
 
         User::truncate();
-        Type::truncate();
         Project::truncate();
-        Entry::truncate();
         Skill::truncate();
         ProjectSkill::truncate();
         Application::truncate();
         
         User::factory()->count(2)->create();
-        Type::factory()->count(3)->create();
         Project::factory()->count(4)->create();
-        Entry::factory()->count(4)->create();
         Skill::factory()->count(4)->create();
         ProjectSkill::factory()->count(4)->create();
         Application::factory()->count(4)->create();
