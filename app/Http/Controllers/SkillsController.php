5c79d41430c9c72af3b1d3bab682f7720e51e530
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-06-20 16:49:33 -0400

    Add Skills Table

---
+++ app/Http/Controllers/SkillsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
+use App\Models\Skill;
+
+class SkillsController extends Controller
+{
+
+    public function list()
+    {
+        return view('Skills.list', [
+            'Skills' => Skill::all()
+        ]);
+    }
+
+    public function addForm()
+    {
+
+        return view('Skills.add');
+    }
+    
+    public function add()
+    {
+
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $skill = new Skill();
+        $skill->title = $attributes['title'];
+        $skill->description = $attributes['description'];
+        $skill->save();
+
+        return redirect('/console/Skills/list')
+            ->with('message', 'Skill has been added!');
+    }
+
+    public function editForm(Skill $skill)
+    {
+        return view('Skills.edit', [
+            'skill' => $skill,
+            'description' => $description
+        ]);
+    }
+
+    public function edit(Skill $skill)
+    {
+
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $skill->title = $attributes['title'];
+        $skill->save();
+
+        return redirect('/console/Skills/list')
+            ->with('message', 'Skill has been edited!');
+    }
+
+    public function delete(Skill $skill)
+    {
+        $skill->delete();
+        
+        return redirect('/console/Skills/list')
+            ->with('message', 'Skill has been deleted!');        
+    }
+
+}
